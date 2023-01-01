83d90b083b1f08701c315e7c5f22892c9480f5a7
Durdali Atılgan <durdaliatilgan@hotmail.com>
2023-01-01 19:41:05 +0300

    Last update

---
+++ WebPageBookCate/Migrations/20230101143836_init.cs
@@ -0,0 +1,39 @@
+﻿
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebPageBookCate.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Books",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Books",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1000)",
+                oldMaxLength: 1000);
+        }
+    }
+}
